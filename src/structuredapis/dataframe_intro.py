3c7233348793115eb908907695f485376d4af9fd
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2026-03-13 16:45:20 +0530

    Configured in PyCharm

--- src/structuredapis/dataframe_intro.py
+++ src/structuredapis/dataframe_intro.py
@@ -1,8 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import desc
 
-# Testing Push
-if __name__ == "__main__": 
+if __name__ == "__main__":
     spark = SparkSession.builder.appName("DataFrameIntro").getOrCreate()
     flights_df = spark.read.csv("/Users/bapu/Public/SourceData/flights_data/2015-summary.csv", header=True, inferSchema=True)
     transformed_df = (flights_df.groupBy("DEST_COUNTRY_NAME")
