a069f3549c27752d2f161f1d752b4f03481b218b
bibhuti <bhusan.bibhutimohapatra@gmail.com>
2026-03-15 09:07:52 +0530

    DataFrame Introduction

--- src/structuredapis/dataframe_intro.py
+++ src/structuredapis/dataframe_intro.py
@@ -1,15 +1,23 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import desc
+from pyspark.sql.types import *
 
-#Test
 if __name__ == "__main__":
-    spark = SparkSession.builder.appName("DataFrameIntro").getOrCreate()
-    flights_df = spark.read.csv("/Users/bapu/Public/SourceData/flights_data/2015-summary.csv", header=True, inferSchema=True)
-    transformed_df = (flights_df.groupBy("DEST_COUNTRY_NAME")
-                      .sum("count")
-                      .withColumnRenamed("sum(count)","destination_total")
-                      .sort(desc("destination_total"))
-                      .limit(5))
-    transformed_df.show()
+    spark = (SparkSession
+             .builder
+             .appName("DataFrameIntro")
+             .getOrCreate())
 
-    
+    # Applying schema manually
+    my_schema = StructType([
+        StructField("DEST_COUNTRY_NAME", StringType(), False, metadata={"description": "Destination country name"})
+        ,StructField("ORIGIN_COUNTRY_NAME", StringType(), False, metadata={"description": "Origin country name"})
+        , StructField("count", LongType(), nullable=False, metadata={"description": "Flight counts"})
+    ])
+
+    flights_df = (spark
+                  .read
+                  .schema(my_schema)
+                  .csv("/Users/bapu/Public/SourceData/flights_data/2015-summary.csv"
+                       , header=True))
+
+    flights_df.printSchema()
